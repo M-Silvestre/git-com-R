4bcbf498e6247790eb989d2830ea29b7b87723d9
Matteus Silvestre <matteussilvestre@gmail.com>
2019-09-13 14:04:39 -0300

    Atualiza arquivo Grafico_Normal.R

--- Grafico_normal.R
+++ Grafico_normal.R
@@ -3,7 +3,7 @@ plot.window(xlim = c(-5,5), ylim = c(-0,1))
 grid(lwd = 2)
 abline(h = 0, v = 0, lwd = 2)
 axis(1, at = -5:5, cex.axis = .8, font = 1)
-axis(2, at = -0:1, cex.axis = .8, font = 1)
+axis(2, at = 0:1, cex.axis = .8, font = 1)
 title(main = "Gráfico de V.A. Normal")
 title(xlab = "Eixo X"); title(ylab = "Eixo Y")
 
